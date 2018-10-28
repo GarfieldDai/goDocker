651323b74e79b19e523eeeadb54e5daffb9a7e10
Garfield Dai <daizhiqiang@coding.net>
2018-10-28 22:59:12 +0800

    Add delve.

--- src/hello/run.sh
+++ src/hello/run.sh
@@ -4,4 +4,4 @@ echo "Building image"
 docker build -t helloserver .
 
 echo "Running container"
-docker run -p 8080:8080 -d helloserver
+docker run -p 8080:8080 -p 40000:40000 --cap-add SYS_PTRACE --security-opt apparmor=unconfined helloserver
